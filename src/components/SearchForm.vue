08abc02ff555ccfe2d32ce4e96e5bcda95a3cf0d
Kayla <frankumk@gmail.com>
2021-07-31 11:03:19 -0700

    basic form built

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,14 +1,48 @@
 <template>
+
+  <form id='car-search' v-on:submit="search">
+
+    <div class='form-field'>
+      <label for='make'>Make: </label>
+      <input type='text' name='make' v-model='form.make'/>
+    </div>
+
+    <div class='form-field'>
+      <label for='model'>Model: </label>
+      <input type='text' name='model' v-model='form.model'/>
+    </div>
+
+    <div class='form-field'>
+      <label for='year'>Year: </label>
+      <input type='text' name='year' v-model='form.year'/>
+    </div>
+
+    <div class='form-field'>
+      <label for='budget'>Budget: </label>
+      <input type='text' name='budget' v-model='form.budget'/>
+    </div>
+
+    <button type="submit">Search</button>
+
+  </form>
   
 </template>
 
 <script>
 
   export default{
-  data: () => ({
-    make: '',
-    model: '',
-    budget: '',
-    year: ''
-  })
+    data: () => ({
+      form: {
+        make: '',
+        model: '',
+        budget: '',
+        year: ''
+      }
+    }),
+    methods: {
+      search(){
+
+      }
+    }
+  }
 </script>
